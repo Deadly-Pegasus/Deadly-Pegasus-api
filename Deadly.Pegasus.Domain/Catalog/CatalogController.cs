afa39b9951247c1c0cb1844ea75e388559ac720e
Slava Makarov <makarov.7@osu.edu>
2025-10-23 19:12:52 -0400

    Catalog API

---
+++ Deadly.Pegasus.Domain/Catalog/CatalogController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Deadly.Pegasus.Domain.Catalog;
+
+namespace Deadly.Pegasus.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CatalogController : ControllerBase
+    {
+        
+    }
+}
